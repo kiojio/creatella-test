c72ec8d18e96eb7cc96cfdd1450aec0ddb18f1a4
ahmadTri <ahmadtri26@gmail.com>
2020-12-25 12:09:33 +0700

    -layout bootstrap -load data products

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,6 +6,8 @@ type Props = {
     title?: string
 }
 
+const adsValue = Math.floor(Math.random()*1000); 
+
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
     <div>
         <Head>
@@ -13,16 +15,14 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
             <meta charSet="utf-8" />
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
-        <header
-            
-        >
+        <header className="container">
             <h1>Products Grid</h1>
 
             <p>Here you're sure to find a bargain on some of the finest ascii available to purchase. Be sure to peruse our selection of ascii faces in an exciting range of sizes and prices.</p>
 
-            <p>But first, a word from our sponsors:</p><img className="ad" src="/ads/?r=' + Math.floor(Math.random()*1000) + '"/>
+            <p>But first, a word from our sponsors:</p><img className="ad" src={`http://localhost:3000/ads/?r=${adsValue}`}/>
         </header>
-        <section className="main">
+        <section className="main container">
             {children}
         </section>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,68 @@
 import Head from 'next/head';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
+import axios from 'axios';
 
-export default function Home() {
-  const [products, setProducts] = useState([]);
+const fetchData = async () => await axios.get('http://localhost:3000/api/products')
+  .then(res => ({
+    error: false,
+    dataProduct: res.data,
+  }))
+  .catch(() => ({
+      error: true,
+      dataProduct: null,
+    }),
+);
 
+const Home = ({dataProduct, error}) => {
+  const [products, setProducts] = useState(dataProduct.slice(0, 10));
+  const [isLoading, setIsLoading] = useState(false);
+  const [limit, setLimit] = useState(10);
+
+  const refresh = () =>{
+    setIsLoading(true);
+    let oldData = dataProduct;
+    let newData = dataProduct.slice(limit, limit+10);
+    let combine = [...products, ...newData];
+    console.log("check", {oldData, newData, products, combine});
+    setTimeout(() => {
+      setProducts(combine);
+      setLimit(limit+10);
+      setIsLoading(false);
+    }, 1000);
+  }
 
   return (
     <Layout title="Product List">
       <div>
         <h1>Product</h1>
+        <button
+          onClick={refresh}
+        >Test</button>
+        {!error && dataProduct && (
+            products.map((data, key) => (
+              <div className="card" style={{width: 288}}>
+                <p className="align-self-center">{data.face}</p>
+                <div className="card-body">
+                  <h5 className="card-title">{data.price}</h5>
+                  <p className="card-text">{data.date}</p>
+                </div>
+              </div>
+            ))
+          )
+        }
       </div>
     </Layout>
   );
 }
+
+export const getStaticProps = async () => {
+  const data = await fetchData();
+
+  return {
+    props: data,
+  };
+}
+
+export default Home;
+
